3530c249975181d477487fc8e7c5f5df2dcdb451
Ramin Bakhshpour <ramin.bp@gmail.com>
2013-02-22 23:52:04 +0330

    Update Sign.py

--- Sign.py
+++ Sign.py
@@ -6,7 +6,7 @@ class SignCommand(sublime_plugin.TextCommand):
         #Loading UserName and AuthorName
         setting = sublime.load_settings(__name__ + '.sublime-settings')
         #Stamp stringBuffer
-        stampStringBuffer = '/**\n' + ' * Created with Sublime Text 2\n'\
+        stampStringBuffer = '/**\n' + ' * Created by Sublime Text 2\n'\
         + ' * User    : ' + setting.get('user_name') + '\n'\
         + ' * Date    : ' + datetime.datetime.now().strftime("%Y/%m/%d")\
         + '\n'\
